8b34261d1a476f61a22dd3abeaab5a114e79f5d1
Haythem Sellami <sellamihaythem96@gmail.com>
2019-11-06 18:18:08 +0100

    fix token symbol

--- contracts/AlkemiToken.sol
+++ contracts/AlkemiToken.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC20/ERC20Mint
 
 contract AlkemiToken is Initializable, ERC20Detailed, ERC20Mintable {
   function initialize(address _sender) public initializer {
-    ERC20Detailed.initialize("Alkemi Token", "ALT", 18);
+    ERC20Detailed.initialize("Alkemi Token", "ALK", 18);
 
     ERC20Mintable.initialize(_sender);
   }
