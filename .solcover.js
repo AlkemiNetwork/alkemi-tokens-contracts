6e1a405a435e7996913d4e92f3e9232dfbaa91cc
Haythem Sellami <sellamihaythem96@gmail.com>
2020-01-07 17:10:01 +0100

    CD-222 migration scripts using openzeppelin cli

--- .solcover.js
+++ .solcover.js
@@ -2,7 +2,7 @@ module.exports = {
     norpc: true,
     copyPackages: [],
     buildDirPath: '/build/contracts',
-    testCommand: 'ganache-cli --port 8555 --gasPrice 0x01 --gasLimit 0xfffffffffff > /dev/null & truffle test --network coverage',
+    testCommand: 'ganache-cli --port 8555 --gasPrice 0x01 --gasLimit 0xfffffffffff > /dev/null & npm run compile && npm run link-dependencies && npm run push-project coverage && truffle test --network coverage',
     skipFiles: [
         'Migrations.sol'
     ]
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,10 +1,26 @@
-var alkemiPrime = artifacts.require("AlkemiPrime");
-var alkemiToken = artifacts.require("AlkemiToken");
+const { scripts, ConfigManager } = require("@openzeppelin/cli");
+const { add, push, create } = scripts;
 
-module.exports = (deployer, accounts) => {
-  deployer.then(async() => {
-    await deployer.deploy(alkemiToken, accounts[0]);
+var AlkemiPrime = artifacts.require("AlkemiPrime");
+var AlkemiToken = artifacts.require("AlkemiToken");
 
-    await deployer.deploy(alkemiPrime, accounts[0]);
+async function deploy(options) {
+  add({ 
+      contractsData: [
+          { name: 'AlkemiPrime', alias: 'AlkemiPrime' },
+          { name: 'AlkemiToken', alias: 'AlkemiToken' }
+      ]
   });
+
+  await push(options);
+
+  let alkemiToken = await create(Object.assign({ contractAlias: 'AlkemiToken' }, options));
+  let alkemiPrime = await create(Object.assign({ contractAlias: 'AlkemiPrime' }, options));
+}
+
+module.exports = function(deployer, networkName, accounts) {
+  deployer.then(async () => {
+      const { network, txParams } = await ConfigManager.initNetworkConfiguration({ network: networkName, from: accounts[0] });
+      await deploy({ network, txParams });
+  })
 }
